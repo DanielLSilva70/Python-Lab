eb644cc4258611dd2e4575a3ae01f3e1bd2616b6
Daniel Silva <daniel@Daniels-MacBook-Air.local>
2022-02-03 14:39:20 -0600

    part 2 done

---
+++ data_presenter.py
@@ -0,0 +1,20 @@
+cup_cakes = open('CupcakeInvoices.csv')
+
+# for row in cup_cakes:
+#     print(row)
+
+# for row in cup_cakes:
+#     values = row.split(',')
+#     print(values[2])    
+
+# for row in cup_cakes:
+#     values = row.split(',')
+#     total = int(values[3]) * float(values[4])
+#     print(total)
+
+total = 0
+
+for row in cup_cakes:
+    values = row.split(',')
+    total =  total + int(values[3]) * float(values[4])
+    print(total)
